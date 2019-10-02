fdee58fccf718bd0ae729ee7d6cf4b6f586d156b
Manish Kumar <53016829+ManishKumar1399@users.noreply.github.com>
2019-10-02 13:02:24 +0530

    Add files via upload

---
+++ 3 deta type.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=4, b=5;
+    int c;
+    c=a+b;
+    printf("The value of c is :%d",c);
+    printf("\nThe sum of %d and %d is %d",a,b,c);
+
+}
